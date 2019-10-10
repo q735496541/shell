5443e9cb75a18f55e9cc7d27e50e693519dd26f5
q735496541 <735496541@qq.com>
2019-10-10 17:21:21 +0800

    your name

---
+++ sh-01.sh
@@ -0,0 +1,7 @@
+#Please input your name
+#Your name has first name and last name
+########################################
+#!/bin/bash
+read -p "Please input your first name:" Aa
+read -p "Please input your last name:" Bb
+echo -e "\n your full name is $Aa$Bb \n"
